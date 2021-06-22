9d54542f42b9d94378bb034605264e71f7155ad0
imanx <imannamix@gmail.com>
2021-06-22 11:03:12 +0430

    impl placeholder indexing

--- src/main/kotlin/impl/DefaultDeserializer.kt
+++ src/main/kotlin/impl/DefaultDeserializer.kt
@@ -1,6 +1,7 @@
 package io.github.imanx.impl
 
 import org.json.JSONObject
+import java.util.regex.Pattern
 
 
 class DefaultDeserializer {
@@ -22,12 +23,29 @@ class DefaultDeserializer {
 
                 val keyName = (parentKey?.let { "${parentKey}_$key" } ?: key).toLowerCase()
 
-                map[keyName] = get(key).toString()
+                map[keyName] = get(key)
+                    .toString()
+                    .toPlaceHolderFormat()
             }
         }
 
         return map
     }
 
+
+    private fun String.toPlaceHolderFormat(): String {
+        var newString = this
+        Pattern.compile("\\{([^}]+)}")
+            .run { matcher(this@toPlaceHolderFormat) }
+            .apply {
+                var index = 0
+                while (find()) {
+                    newString = newString.replace(group(), "{$index}")
+                    index++
+                }
+            }
+
+        return newString
+    }
 
 }
